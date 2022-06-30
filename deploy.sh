e823d6992605c533fb0c0fe388a88874e73e9c7e
Fernando Poli <fernandoandrespoli@gmail.com>
2022-06-30 09:42:22 -0300

    Chaning the deplsoy

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:fernandopoli/rick-and-morty.git main:gh-pages
+git push -f git@github.com:https://fernandopoli.github.io/rick-and-morty main:gh-pages
 
 cd -
